9c6642e346a57d89910c7b073092069d95a9bf98
TimRudy <tim@roi.com>
2022-07-14 19:33:29 +0000

    Add button style

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ app.engine('hbs', engine({
     defaultView: 'default'
 }));
 
+// and add styling
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+
 // set up the parser to get the contents of data from html forms 
 // this would be used in a POST to the server as follows:
 // app.post('/route', urlencodedParser, (req, res) => {}
